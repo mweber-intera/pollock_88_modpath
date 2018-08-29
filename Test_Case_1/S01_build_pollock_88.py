278c074933d63b5340975b6def3f6db28935e923
rosskush <ross.kush@gmail.com>
2018-08-29 09:19:22 -0500

    modified TC1

--- Test_Case_1/S01_build_pollock_88.py
+++ Test_Case_1/S01_build_pollock_88.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-model_ws = os.path.join('pollock_model_ex1')
+model_ws = os.path.join('workspace')
 if not os.path.exists(model_ws): os.mkdir(model_ws)
 gw_codes = os.path.join('..','gw_codes')
 exe = os.path.join(gw_codes,'mf2k-chprc08spl.exe')
@@ -64,7 +64,6 @@ wel = flopy.modflow.ModflowWel(mf,stress_period_data=wel_spd) # well package
 
 # CHD
 chd_df = pd.read_csv('chb_t1.csv') # read in csv with pandas
-# print(chd_df.head()) # printing with .head() lets you see the first 5 rows of a dataframe
 
 chd_dat = []
 for i, vals in chd_df.iterrows(): # fancy for loop that returns the index in i, and the values in vals
@@ -75,7 +74,6 @@ for i, vals in chd_df.iterrows(): # fancy for loop that returns the index in i, 
 
 
 chd_spd = {0:chd_dat} # only need do do in the first stress period since modflow uses the previous stress period if there is nothing in the current stress period.
-# print(chd_spd)
 chd = flopy.modflow.ModflowChd(mf,stress_period_data=chd_spd)
 
 # write modflow files
@@ -128,7 +126,6 @@ df = pd.DataFrame({'ParticleID':step4,
                    'GlobalY':step3})
 
 
-
 # equation for calculating the line length
 def calc_line_length(globalx, globaly):
     line_length = np.sqrt((globalx**2)+(globaly**2))
@@ -136,49 +133,41 @@ def calc_line_length(globalx, globaly):
 
 # get the 0 day average from mppth
 xlist0d = df.loc[(df['Time']==0.000000000000000E+00), ['ParticleID','Time','GlobalX','GlobalY']]
-# print(xlist0d)
 
 # get the 2500 day average from mppth
 xlist25h = df.loc[(df['Time']==0.250000000000000E+04), ['ParticleID','Time','GlobalX','GlobalY']]
 xlist25h['length']=np.sqrt((xlist25h['GlobalX']**2)+(xlist25h['GlobalY']**2))
 av_len_25h = np.average(xlist25h['length'])
-# print(av_len_25h)
 
 # get the 5000 day average from mppth
 xlist5k = df.loc[(df['Time']==0.500000000000000E+04), ['ParticleID','Time','GlobalX','GlobalY']]
 xlist5k['length']=np.sqrt((xlist5k['GlobalX']**2)+(xlist5k['GlobalY']**2))
 av_len_5k = np.average(xlist5k['length'])
-# print(av_len_5k)
 
 # get the 7500 day average from mppth
 xlist75h = df.loc[(df['Time']==0.750000000000000E+04), ['ParticleID','Time','GlobalX','GlobalY']]
 xlist75h['length']=np.sqrt((xlist75h['GlobalX']**2)+(xlist75h['GlobalY']**2))
 av_len_75h = np.average(xlist75h['length'])
-# print(av_len_75h)
 
 mppth_all = [av_len_25h, av_len_5k, av_len_75h]
 
 # get the 2500 day average from digitized file
 dig_xlist25h = digitized.loc[(digitized['days']==2500), ['distance']]
 dig_av_len_25h = np.average(dig_xlist25h['distance'])
-# print(dig_av_len_25h)
 
 # get the 5000 day average from digitized file
 dig_xlist5k = digitized.loc[(digitized['days']==5000), ['distance']]
 dig_av_len_5k = np.average(dig_xlist5k['distance'])
-# print(dig_av_len_5k)
 
 # get the 7500 day average from digitized file
 dig_xlist75h = digitized.loc[(digitized['days']==7500), ['distance']]
 dig_av_len_75h = np.average(dig_xlist75h['distance'])
-# print(dig_av_len_75h)
 
 dig_all = [dig_av_len_25h, dig_av_len_5k, dig_av_len_75h]
 
 # calculate percent difference
 perd_tc1 = []
 perdl=[perd_tc1.append(((dig_all[i] - mppth_all[i])/(dig_all[i]+mppth_all[i]/2))*100) for i in range(0,len(mppth_all))]
-# print(perd_tc1)
 
 # determine pass/fail
 
@@ -221,10 +210,6 @@ for time in times:
     extent=(0,Lx,0,Ly)
     if time != 0:
         head = headobj.get_data(totim=times[-1])
-        # CS = plt.contour(np.flipud(head[0]), extent=extent, color='k')
-        # plt.clabel(CS, inline=1, fontsize=10)
-        # plt.imshow(head[0],cmap='cubehelix',extent=extent)
-        # plt.colorbar()
 
     modelmap = flopy.plot.ModelMap(model=mf, layer=0, ax=ax)
     lc = modelmap.plot_grid(color='c',alpha=.25)
--- Test_Case_1/runme.bat
+++ Test_Case_1/runme.bat
@@ -1,4 +1,4 @@
 rem runme.bat for test case 1
 rem this will produce the results in output
-python build_pollock_88.py 
+python S01_build_pollock_88.py 
 
